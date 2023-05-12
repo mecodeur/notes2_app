ff0dae1f415fbaf2a65747c4152d6fdd7e50b657
mecodeur <mehdi.el2023@icloud.com>
2023-05-12 21:01:57 +0100

    Add notes cubit and create states

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes2_app/constants.dart';
-import 'package:notes2_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes2_app/simple_bloc_observer.dart';
 import 'package:notes2_app/views/notes_wiew.dart';
 
