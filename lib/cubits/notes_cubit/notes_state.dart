4446ce0f8a4fd072a8419882a896c961fcd1db94
mecodeur <mehdi.el2023@icloud.com>
2023-05-12 23:14:15 +0100

    display notes part one

--- lib/cubits/notes_cubit/notes_state.dart
+++ lib/cubits/notes_cubit/notes_state.dart
@@ -5,16 +5,16 @@ abstract class NotesState {}
 
 class NotesInitial extends NotesState {}
 
-class NotesLoading extends NotesState {}
+/*class NotesLoading extends NotesState {}
 
 class NotesSuccess extends NotesState {
   final List<NoteModel> notes;
 
   NotesSuccess(this.notes);
-}
+}*/
 
-class NotesFailure extends NotesState {
+/*class NotesFailure extends NotesState {
   final String msgError;
 
   NotesFailure(this.msgError);
-}
+}*/
--- lib/widgets/notes_list_view.dart
+++ lib/widgets/notes_list_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes2_app/cubits/notes_cubit/notes_cubit.dart';
+import 'package:notes2_app/models/note_model.dart';
 
 import 'custom_note_item.dart';
 
@@ -7,8 +10,17 @@ class NotesListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(padding: EdgeInsets.zero, itemBuilder: (context, index) {
-      return CustomNoteItem();
-    });
+    return BlocBuilder<NotesCubit, NotesState>(
+      builder: (context, state) {
+        List<NoteModel> notes = BlocProvider.of<NotesCubit>(context).notes!;
+
+        return ListView.builder(
+            padding: EdgeInsets.zero,
+            itemCount: notes.length,
+            itemBuilder: (context, index) {
+              return CustomNoteItem();
+            });
+      },
+    );
   }
 }
